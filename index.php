7ae7222da10ff6adf87ebb83c368d849b6e5d7e3
yannick forget <yannick forget>
2017-04-13 00:27:35 +0200

    Made the first little bit of design

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Coffee and Coding | Home</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <nav class="clearfix">
+            <img class="nav-logo" src="img/cnc.png" height="50" width="50" />
+            <h4 class="nav-text">Coffee and Coding</h4>
+    		<ul class="clearfix">
+                <li><a href="#beginning">Home</a></li>
+    			<li><a href="#about">About</a></li>
+    			<li><a href="#">Team</a></li>
+    			<li><a href="#">Faq's</a></li>
+    			<li><a href="#">Sponsors</a></li>
+    			<li><a href="#">Venue</a></li>
+    			<li><a href="#">Contact</a></li>
+    		</ul>
+    		<a href="#" id="pull"></a>
+    	</nav>
+
+        <section id="beginning">
+            <img class="mediacentrale" src="img/mediacentrale.jpg" />
+
+            <div class="register">
+                <h2>24 hours of creating and inspiring</h2>
+
+                <a href="https://goo.gl/forms/DXDqgE3LWUsB7Dhv2">Register now</a>
+            </div>
+        </section>
+        
+        <section id="about container">
+                    
+        </section>
+
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+
+    </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(function() {
+    var pull 		= $('#pull');
+        menu 		= $('nav ul');
+        menuHeight	= menu.height();
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+    
+   $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+   });
+
+});
