46ab40602bd7bebcf22663d80482b730b108a36f
yannick forget <yannick forget>
2017-04-13 23:29:16 +0200

    finished a bit of design

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
                 <li><a href="#beginning">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#team">Team</a></li>
-                <li><a href="#">Faq's</a></li>
-                <li><a href="#">Sponsors</a></li>
+                <li><a href="#faq">Faq's</a></li>
+                <li><a href="#sponsors">Sponsors</a></li>
                 <li><a href="#">Venue</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
@@ -43,6 +43,7 @@
                 professionals will judge your prototype. You can will serveral prizes and meet a lot of new people! in short, just an amazing experience that you can be proud of!
             </p>
         </section>
+        
         <section id="team" class="container">
             <div class="row">
                 <h3>Team</h3>
@@ -112,6 +113,174 @@
             </div>
             
         </section>
+        
+        <section id="faq" class="container">
+            <div class="row">
+                <h3>Faq's</h3>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>What in cheesename is a hackathon?!?</h3>
+                            <p>A hackathon is an awesome event where a lot of people come together and create and invent new awesome things during 24-48 hours.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>What should I bring?</h3>
+                            <p>Your student id, yourself and a good mood, your laptop and charger. We will provide you with internet, a table for your team and a lot of food and drinks (including coffee)</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>The name of the event is coffee and coding so will there be coffee?</h3>
+                            <p>Yes, More than you can ever imagine</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Where and when will this event take place?</h3>
+                            <p>We are proud to announce that the event is going take place at the Mediacentrale in Groningen from the 25 till the 26th of july 2017. The event will be 24 approximately.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>This sounds awesome, where can I register?</h3>
+                            <p>At the top of the page there is a big fat "Register now" button. You can click that if you want to participate :)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Will there be a code of conduct</h3>
+                            <p>Yup, the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">MLH code of conduct</a>. We strictly enforce it. If you want to know more about the hackathon rules and the code of conduct than click on the "register now" button at the top of the page.</p>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Who can participate?</h3>
+                            <p>Every college, university and high school student that is over 14 years of age at the start of the event is welcome to join us. You are also welcome to join us if you graduated lesser than 12 months ago.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Will there be a schedule?</h3>
+                            <p>Yes ofcourse! We're still working on it. When it's done we will publish it on our website.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>How big can the teams be?</h3>
+                            <p>Teams can be up to 6 people. There is no minimum number of participants in a team. </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>How can I register my team?</h3>
+                            <p>All teams will be registered at the hackathon. There will be some time to meet new people and search for team members.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Do I need a team to register?</h3>
+                            <p>No, you can just come and we will try and help you to find an awesome team! But in case you're not able to find a team be prepared to hack alone.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+
+                        <div class="caption">
+                            <h3>Will there be a place to sleep?</h3>
+                            <p>We will have a "chill room" for you to take a quick nap or just to chill out but we don't advice you to sleep there if you want a good night of sleep. If you want to use this room to take a nap we advice you to bring your sleepingbag and pillow. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+        
+        <section id="sponsors" class="container">
+            <h3>Sponsors</h3>
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <a href="https://osso.nl"><img src="img/osso.png"></a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <a href="http://waarmediacentrale.nl"><img src="img/waar.png"></a>
+                </div>
+            </div>
+                
+            <h3>Also want to sponsor? info@coffeeandcoding.nl</h3>
+        </section>
+        
+  
+        
+        <section id="venue" class="container">
+            <h3>Venue</h3>
+            <img style="display: block; margin: 0 auto; text-align: center; max-width: 100%;" src="img/mediacentrale2.jpg">
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <iframe style="max-width: 100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1380.5155442785663!2d6.588785070940265!3d53.20398024665834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c82d4dcc533367%3A0x445da9c42b04d8d!2sMediacentrale!5e0!3m2!1sen!2snl!4v1492116625127" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <h4 style="text-align:center;">Mediacentrale Groningen<br> Helperpark 270-298 <br>9723 ZA Groningen </h4>
+                    <p>We are proud to announce that the Mediacentrale in Groningen will be our venue for this event. The mediacentrale is a great place for events like this. A lot of IT and Media companies are located here. The Mediacentrale is a 3 min walk from Europapark Groningen trainstation. If you're coming by bus you can take busline 5 (direction Europapark) and get out at Europapark. For the people that really like to walk: you can take a 20 minute walk from the central station of Groningen.</p>
+                </div>
+            </div>
+                
+        </section>
+        
+        <section id="contact">
+            <h3>Still have a question or do you want to sponsor? <br> You can always shoot us an <a href="mailto: info@coffeeandcoding.nl">e-mail!</a></h3>
+            <div class="row">
+                <div class="col-md-3 col-sm-12">
+                    <ul id="social">
+                        <li><a href="https://twitter.com/coffee_n_coding"><img style="display: block; margin: 0 auto;" src="img/twitter.png"></a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </section>
+        
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/script.js"></script>
