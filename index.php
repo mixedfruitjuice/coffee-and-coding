1724bb590fb7af5aefe9b37d36928f805a6ff898
yannick forget <yannick forget>
2017-04-13 20:43:37 +0200

    Updated team section

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,43 +9,112 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/normalize.css">
         <link href="css/bootstrap.min.css" rel="stylesheet">
-
     </head>
     <body>
-
         <nav class="clearfix">
             <img class="nav-logo" src="img/cnc.png" height="50" width="50" />
             <h4 class="nav-text">Coffee and Coding</h4>
-    		<ul class="clearfix">
+            <ul class="clearfix">
                 <li><a href="#beginning">Home</a></li>
-    			<li><a href="#about">About</a></li>
-    			<li><a href="#">Team</a></li>
-    			<li><a href="#">Faq's</a></li>
-    			<li><a href="#">Sponsors</a></li>
-    			<li><a href="#">Venue</a></li>
-    			<li><a href="#">Contact</a></li>
-    		</ul>
-    		<a href="#" id="pull"></a>
-    	</nav>
-
+                <li><a href="#about">About</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#">Faq's</a></li>
+                <li><a href="#">Sponsors</a></li>
+                <li><a href="#">Venue</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <a href="#" id="pull"></a>
+        </nav>
         <section id="beginning">
             <img class="mediacentrale" src="img/mediacentrale.jpg" />
-
             <div class="register">
                 <h2>24 hours of creating and inspiring</h2>
-
                 <a href="https://goo.gl/forms/DXDqgE3LWUsB7Dhv2">Register now</a>
             </div>
         </section>
-        
-        <section id="about container">
-                    
+        <section id="about" class="container">
+            <h3>Do you like creating stuff and are you a dreamer but also someone who likes to turn his dreams into reality?</h3>
+            <p>Then you are welcome to join these 24 hours on the 25th of july till the 26th of july. Every College and high school student is welcome to join us and create and pitch their idea.
+                Did you always had an amazing idea for an ultra supersonic app or a new trailblazing website or piece of software then these 24 hours are an amazing oppurtunity for you. You'll join
+                a lot of other nerds/geeks/super hero's/designers etc. to turn your concept into an amazing prototype that you can pitch at the end of the hackathon.
+                <br>
+                <br>
+                There will be sponsors and mentors from other companies to make these 24 hours an amazing success for you. At the end of the hackathon you'll have to pitch your idea with your team and a jury of
+                professionals will judge your prototype. You can will serveral prizes and meet a lot of new people! in short, just an amazing experience that you can be proud of!
+            </p>
         </section>
-
-
+        <section id="team" class="container">
+            <div class="row">
+                <h3>Team</h3>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/yannick.jpg">
+                        <div class="caption">
+                            <h3>Yannick Forget</h3>
+                            <p>Head of organization</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/jaydee.jpg">
+                        <div class="caption">
+                            <h3>Jaydee Alkema</h3>
+                            <p>Promotions hero</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/tom.jpg">
+                        <div class="caption">
+                            <h3>Tom Roelfsema</h3>
+                            <p>Hacker experience master</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/danique.jpg">
+                        <div class="caption">
+                            <h3>Danique Bruintjes</h3>
+                            <p>Another promotions hero</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/mathijs.jpg">
+                        <div class="caption">
+                            <h3>Mathijs Westerhof</h3>
+                            <p>Administration hero</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/justen.jpg">
+                        <div class="caption">
+                            <h3>Justen Jobsis</h3>
+                            <p>Designer and administration hero</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="img/test.jpg">
+                        <div class="caption">
+                            <h3>Colin</h3>
+                            <p>Sponsorships manager</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/script.js"></script>
-
     </body>
 </html>
+
